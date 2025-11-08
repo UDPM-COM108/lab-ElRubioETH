6fb5a6425ef09adf9195ce7cda9579f8ba437072
ElRubioETH <henkenmanybruh@gmail.com>
2025-11-08 13:55:34 +0700

    Edit Lab2 B3

--- Lab2/Lab2_B3.C
+++ Lab2/Lab2_B3.C
@@ -12,6 +12,26 @@ int main() {
 
     printf("Chu vi hinh tron = %.2f\n", chuviHT);
     printf("Dien tich hinh tron = %.2f\n", dientichHT);
-
+  
     return 0;
 }
+
+
+//  #include <iostream>
+//  using namespace std;
+
+//  int main() {
+//     float r;
+//     const float PI = 3.14;
+
+//     cout << "Nhap ban kinh: ";
+//     cin >> r;
+
+//     float chuviHT = 2 * PI * r;
+//     float dientichHT = PI * r * r;
+
+//     cout << "Chu vi hinh tron = " << chuviHT << endl;
+//     cout << "Dien tich hinh tron = " << dientichHT << endl;
+
+//     return 0;
+// }
